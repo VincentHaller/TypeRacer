3818818922c949966b2ef2e779b2de24bbbe957d
VincetHaller <vincenthaller2@gmail.com>
2023-12-22 19:42:09 +0100

    ok finally making some progress

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -1,12 +1,50 @@
-#%%
+#%% Imports
+import sys
+sys.path.append('..')
+
 import numpy as np
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import json
+import requests
+
 from bs4 import BeautifulSoup
 
 # %%
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
+type_url = json.load(open("config/url.json"))
+
+#%%
+url = f"{type_url['url_start']}{type_url['user']}{type_url['url_end']}"
+
+r = requests.get(url)
+# %%
+soup = BeautifulSoup(r.content, 'html.parser')
+# %%
+print(soup.title)
+# %%
+s = soup.find('div', class_='profileTableHeaderUniverse')
+# %%
+lines =s.find_all_next('a')
+# %%
+for line in lines:
+	print(line.text)
+# %%
+print(r.content)
+# %%
+print(str(r.content))
+# %%
+s.find_all_next('a')
+# %%
+
+lines[0].__str__.contains('details')
+# %%
+soup.find_all('a')[0].text
+# %%
+soup.find_all('div','profileTableHeaderRaces' )
+# %%
+
+race_number = soup.find_all('div', 'profileTableHeaderUniverse')
+
+# %%
+for num in race_number:
+	print(int(num.text.translate({'\n':None, ' ':None})))
 # %%
