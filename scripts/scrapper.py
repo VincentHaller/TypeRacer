723360526f86aa7a5c31bc731bef4d709ac3cd38
VincetHaller <vincenthaller2@gmail.com>
2024-01-17 23:22:46 +0100

    scrapper only missing date

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -18,4 +18,21 @@ plt.show()
 plt.plot(np_data[:, 0], np_data[:, 2])
 plt.title('Accuracy')
 plt.show()
+
+plt.scatter(np_data[:, 2], np_data[:, 1])
+plt.title("Accuracy vs Speed")
+plt.show()
+# %%
+import pandas as pd
+# %%
+df = (
+	pd.DataFrame(np_data, columns=["Race", "WPM", "Accuracy"])
+	.set_index("Race")
+	.sort_index()
+	.rolling(10)
+	.mean()
+)
+
+# %%
+df.plot()
 # %%
